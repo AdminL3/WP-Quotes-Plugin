e397ab40a92374e794cf52b5a408ddf3a33bd890
AdminL3 <l-blu@outlook.de>
2025-01-15 15:23:02 +0100

    added changing and deleting the existing databases

--- wp-quotes/includes/settings.php
+++ wp-quotes/includes/settings.php
@@ -7,15 +7,25 @@ function wp_quotes_settings_page() {
     <p>Welcome to the WP Quotes plugin settings page.</p>
     <p>Upload the database with the quotes:</p>
 
-    <!-- File Upload Form -->
     <form method="post" action="" enctype="multipart/form-data">
         <?php wp_nonce_field('wp_quotes_file_upload', 'wp_quotes_nonce'); ?>
         <label for="db_file">Upload Database File:</label>
         <input type="file" id="db_file" name="db_file" /><br>
         <input type="submit" name="upload_file" value="Upload File" />
     </form>
-    
-    
+    <br>
+    <form method="post" action="">
+        <label for="db_name">Set Database Name:</label>
+        <input type="text" id="db_name" name="name_input" value="<?php echo esc_attr($db_name); ?>" /><br>
+        <input type="submit" name="update_name" value="Enter Database Name" />
+    </form>
+    <br>
+    <form method="post" action="">
+        <label for="db_name">Delete Database:</label>
+        <input type="text" id="db_name" name="name_input" value="/2025/01/quotes.db" /><br>
+        <input type="submit" name="delete_db" value="Delete" />
+    </form>
+
     <?php
 
     if (isset($_POST['upload_file']) && check_admin_referer('wp_quotes_file_upload', 'wp_quotes_nonce')) {
@@ -43,6 +53,23 @@ function wp_quotes_settings_page() {
             }
         } else {
             echo 'No file selected for upload.';
+        }
+    }
+    if (isset($_POST['update_name'])) {
+        $new_name = sanitize_text_field($_POST['name_input']);
+        update_option('wp_quotes_db_name', $new_name);
+
+        echo '<meta http-equiv="refresh" content="0">';
+    }
+    if (isset($_POST['delete_db'])) {
+        $db_name = sanitize_text_field($_POST['name_input']);
+        $upload_dir = wp_upload_dir();
+        $file_path = $upload_dir['basedir'] . $db_name;
+        if (file_exists($file_path)) {
+            unlink($file_path);
+            echo 'File deleted successfully.';
+        } else {
+            echo 'File not found.';
         }
     }
 }
